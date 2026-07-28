e8e6d8b317ee247f6c662cbe2a6f080d015518e0
samarthmulagund <samarth.mulagund2014@gmail.com>
2026-07-28 16:38:09 +0530

    Add files via upload

--- Python_Programs/app.py
+++ Python_Programs/app.py
@@ -9,7 +9,7 @@ st.set_page_config(
     page_title="QuickCalc",
     page_icon="🧮"
 )
-img = get_base64("Python_Programs/images/calc-background.jpeg")
+img = get_base64("Python_Programs/images/calc-backdrop.jpeg")
 st.markdown(
     f"""
     <style>
