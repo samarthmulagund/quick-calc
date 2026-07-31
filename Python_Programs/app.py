b59be59ba4e213b7427366231bfd1fe932fb1f5c
samarthmulagund <samarth.mulagund2014@gmail.com>
2026-07-31 16:34:49 +0530

    Add files via upload

--- Python_Programs/app.py
+++ Python_Programs/app.py
@@ -32,39 +32,54 @@ if language == "English":
     first_num = st.number_input("Give 1st number", step = 1)
     second_num = st.number_input("Give 2nd number", step = 1)
 
-    if st.button("+"):
-         st.write(f"Sum is {first_num + second_num}")
-    elif st.button("-"):
-        st.write(f"Difference is {first_num - second_num}")
-    elif st.button("X"):
-        st.write(f"Product is {first_num * second_num}")
-    elif st.button("÷"):
-        st.write(f"Quotient is {first_num / second_num}")
+    col1, col2, col3, col4 = st.columns(3)
+    with col1:
+        if st.button("+"):
+             st.write(f"Sum is {first_num + second_num}")
+    with col2:
+        if st.button("-"):
+            st.write(f"Difference is {first_num - second_num}")
+    with col3:
+        if st.button("X"):
+            st.write(f"Product is {first_num * second_num}")
+    with col4:
+        if st.button("÷"):
+            st.write(f"Quotient is {first_num / second_num}")
 
 elif language == "ಕನ್ನಡ":
     st.title("ಕ್ವಿಕ್‌ಕ್ಯಾಲ್ಕ್")
     first_num = st.number_input("1 ನೇ ಸಂಖ್ಯೆಯನ್ನು ನೀಡಿ", step=1)
     second_num = st.number_input("2 ನೇ ಸಂಖ್ಯೆಯನ್ನು ನೀಡಿ.", step=1)
 
-    if st.button("+"):
-        st.write(f"ಮೊತ್ತವು {first_num + second_num}")
-    elif st.button("-"):
-        st.write(f"ವ್ಯತ್ಯಾಸವೆಂದರೆ {first_num - second_num}")
-    elif st.button("X"):
-        st.write(f"ಉತ್ಪನ್ನವು {first_num * second_num}")
-    elif st.button("÷"):
-        st.write(f"ಅಂಶವು {first_num / second_num}")
+    col1, col2, col3, col4 = st.columns(3)
+    with col1:
+        if st.button("+"):
+            st.write(f"ಮೊತ್ತವು {first_num + second_num}")
+    with col2:
+        if st.button("-"):
+            st.write(f"ವ್ಯತ್ಯಾಸವೆಂದರೆ {first_num - second_num}")
+    with col3:
+        if st.button("X"):
+            st.write(f"ಉತ್ಪನ್ನವು {first_num * second_num}")
+    with col4:
+        if st.button("÷"):
+            st.write(f"ಅಂಶವು {first_num / second_num}")
 
 if language == "हिंदी":
     st.title("क्विककैल्क")
     first_num = st.number_input("पहला नंबर दें", step = 1)
     second_num = st.number_input("दूसरा नंबर दें", step = 1)
 
-    if st.button("+"):
-         st.write(f"योग है {first_num + second_num}")
-    elif st.button("-"):
-        st.write(f"फ़र्क यह है कि {first_num - second_num}")
-    elif st.button("X"):
-        st.write(f"प्रोडक्ट है {first_num * second_num}")
-    elif st.button("÷"):
-        st.write(f"भागफल है {first_num / second_num}")
+    col1, col2, col3, col4 = st.columns(3)
+    with col1:
+        if st.button("+"):
+             st.write(f"योग है {first_num + second_num}")
+    with col2:
+        if st.button("-"):
+            st.write(f"फ़र्क यह है कि {first_num - second_num}")
+    with col3:
+        if st.button("X"):
+            st.write(f"प्रोडक्ट है {first_num * second_num}")
+    with col4:
+        if st.button("÷"):
+            st.write(f"भागफल है {first_num / second_num}")
